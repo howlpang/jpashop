5ffdc2b3642e74d76ea9f5fe2f5c5cb220c53b50
CYG <bbaa6909@naver.com>
2026-05-26 15:16:20 +0900

    First Commit!

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.server.i18n.AcceptHeaderLocaleContextResolver;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println(data);
+
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<Category>();
+
+    //==비즈니스로직==//
+    /**
+     * stock 증가
+     * @param quantity
+     */
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     * @param quantity
+     */
+
+    public void removeStock(int quantity) {
+    int restStock = this.stockQuantity -= quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("재고 수량이 필요합니다.");
+        }
+        this.stockQuantity = restStock;
+    }
+}
