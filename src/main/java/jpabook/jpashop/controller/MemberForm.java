cb048c8ffea74e3f623c957b0022d9a4de6b2b94
CYG <bbaa6909@naver.com>
2026-05-27 14:54:32 +0900

    웹 구성 초기

---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+
+
+
+@Getter
+@Setter
+public class MemberForm {
+
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+
+}
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,15 +5,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import jakarta.persistence.EntityManager;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
+
     private final EntityManager em;
+
     public void save(Order order) {
         em.persist(order);
     }
+
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
-// public List<Order> findAll(OrderSearch orderSearch) { ... }
+
+    // jpql 자바 코드로 작성할 때 표준으로 작성하는 방법
+    /*public List<Order> findAll(OrderSearch orderSearch) {}*/
 }
